26fd326927f04cd03328ebe4f84ced8bccdf7e64
yagomezm <42954109+yagomezm@users.noreply.github.com>
2018-11-06 22:53:30 -0500

    Update app.js

--- ProyectosSancionadosCongreso/app.js
+++ ProyectosSancionadosCongreso/app.js
@@ -4,7 +4,7 @@ d3.json("jsontotalult.json").then(function(tdat) {
 // Set the dimensions and margins of the diagram
 var margin = {top: 20, right: 10, bottom: 30, left: 180},
     width = 2048 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
+    height =1024 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
